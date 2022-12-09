f8a6d1348f3a4b3c91328f23f732d0972139f2ec
SAStevens <118325820+SAStevens@users.noreply.github.com>
2022-12-08 18:48:27 -0700

    Exercise 2

---
+++ script2.js
@@ -0,0 +1,16 @@
+let userName="Scott"
+const userEmail="saseaa37@gmail.com"
+let password="12345"
+let userAge=33
+let geoLocation="USA"
+let monthlySalary=5000
+
+
+
+
+console.log(userName, userEmail,monthlySalary);
+document.write(`
+<p>User: ${userName}</p>
+<p>Email: ${userEmail} </p>
+<p>Salary ${monthlySalary*12} </p>
+`)
